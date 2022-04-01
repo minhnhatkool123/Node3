43a418b70ad393575eeb1ce871c036d16fbacb64
Nguyen Pham Minh Nhat <nhatnpm@MECORP.LOCAL>
2022-04-01 15:17:25 +0700

    Done

--- services/payment/actions/statisticCustomer.action.js
+++ services/payment/actions/statisticCustomer.action.js
@@ -81,10 +81,79 @@ module.exports = async function (ctx) {
 			],
 		]);
 
+		let dataCustomer = await this.broker.call("v1.order.aggregate", [
+			[
+				{
+					$match: where,
+				},
+				{
+					$group: {
+						_id: {
+							userId: "$userId",
+						},
+						succeeded: {
+							$sum: {
+								$cond: [{ $eq: ["$status", "SUCCESS"] }, 1, 0],
+							},
+						},
+						pending: {
+							$sum: {
+								$cond: [{ $eq: ["$status", "PENDING"] }, 1, 0],
+							},
+						},
+						failed: {
+							$sum: {
+								$cond: [{ $eq: ["$status", "FAILED"] }, 1, 0],
+							},
+						},
+					},
+				},
+				{
+					$sort: { _id: 1 },
+				},
+			],
+		]);
+
+		if (dataCustomer) {
+			let userIdArr = [];
+			let userArr = [];
+			for (let i = 0; i < dataCustomer.length; i++) {
+				userIdArr.push(dataCustomer[i]._id.userId);
+			}
+			userIdArr = [...new Set(userIdArr)];
+			console.log(userIdArr);
+
+			for (const userId of userIdArr) {
+				let user = await this.broker.call(
+					"v1.MiniProgramUserModel.findOne",
+					[
+						{
+							id: userId,
+						},
+						"-_id name email id",
+					]
+				);
+				user.userId = user.id;
+				delete user.id;
+				userArr.push(user);
+			}
+
+			for (let i = 0; i < dataCustomer.length; i++) {
+				for (let j = 0; j < userArr.length; j++) {
+					if (userArr[j].userId === dataCustomer[i]._id.userId) {
+						dataCustomer[i].userInfo = userArr[j];
+					}
+				}
+			}
+		}
+
+		console.log("dataCustomer", dataCustomer);
+
 		return {
 			code: 1000,
 			message: this.__("Thành công"),
 			data,
+			dataCustomer,
 		};
 	} catch (err) {
 		if (err.name === "MoleculerError") throw err;
